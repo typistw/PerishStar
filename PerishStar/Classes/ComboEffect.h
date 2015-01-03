b7acfd00fb43fa128eb69d3627d83d858f4938dd
typistw <typistw@163.com>
2015-01-03 19:43:17 +0800

    特效

---
+++ PerishStar/Classes/ComboEffect.h
@@ -0,0 +1,11 @@
+#ifndef __ComboEffect_H__
+#define __ComboEffect_H__
+
+#include "cocos2d.h"
+USING_NS_CC;
+
+//显示特效(吊炸天之类)
+void showComboEffect(int size,Node* node);
+
+
+#endif //__ComboEffect_H__
